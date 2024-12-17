fbac9af108c16f05222add87f7df0294afa4a953
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-12-16 18:07:03 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -18,6 +18,24 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
 # Título y descripción
 st.title("Predicción de Potencial Comercial de Yacimientos Petroleros")
 st.write("""
